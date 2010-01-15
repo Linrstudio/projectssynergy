aea44b5d7f4429044e8bf0fdf60c6216e7a8e5cb
roenyroeny <roenyroeny@57e7344c-89cc-11de-832f-d15e35d84da2>
2010-01-15 07:46:08 +0000

    *Fixed vector comparation bug

--- Code/Template/Vector.cs
+++ Code/Template/Vector.cs
@@ -10,54 +10,18 @@ namespace SynergyTemplate
         public int X;
         public int Y;
 
-        public Int2(int _X, int _Y)
-        {
-            X = _X; Y = _Y;
-        }
-
-        public static bool operator <(Int2 _A, Int2 _B)
-        {
-            return _A.X < _B.X && _A.Y < _B.Y;
-        }
-        public static bool operator >(Int2 _A, Int2 _B)
-        {
-            return _A.X > _B.X && _A.Y > _B.Y;
-        }
-
-        public static Int2 operator +(Int2 _A, Int2 _B)
-        {
-            return new Int2(_A.X + _B.X, _A.Y + _B.Y);
-        }
-
-        public static Int2 operator -(Int2 _A, Int2 _B)
-        {
-            return new Int2(_A.X - _B.X, _A.Y - _B.Y);
-        }
-
-        public static Int2 operator *(Int2 _A, Int2 _B)
-        {
-            return new Int2(_A.X * _B.X, _A.Y * _B.Y);
-        }
-
-        public static Int2 operator *(Int2 _A, int _B)
-        {
-            return new Int2(_A.X * _B, _A.Y * _B);
-        }
-
-        public static Int2 operator /(Int2 _A, Int2 _B)
-        {
-            return new Int2(_A.X / _B.X, _A.Y / _B.Y);
-        }
-
-        public static Int2 operator /(Int2 _A, int _B)
-        {
-            return new Int2(_A.X / _B, _A.Y / _B);
-        }
-
-        public static Int2 operator -(Int2 _A)
-        {
-            return new Int2(-_A.X, -_A.Y);
-        }
+        public Int2(int _X, int _Y) { X = _X; Y = _Y; }
+        public static bool operator <(Int2 _A, Int2 _B) { return _A.X < _B.X && _A.Y < _B.Y; }
+        public static bool operator >(Int2 _A, Int2 _B) { return _A.X > _B.X && _A.Y > _B.Y; }
+        public static bool operator ==(Int2 _A, Int2 _B) { return _A.X == _B.X && _B.Y == _B.Y; }
+        public static bool operator !=(Int2 _A, Int2 _B) { return !(_A == _B); }
+        public static Int2 operator +(Int2 _A, Int2 _B) { return new Int2(_A.X + _B.X, _A.Y + _B.Y); }
+        public static Int2 operator -(Int2 _A, Int2 _B) { return new Int2(_A.X - _B.X, _A.Y - _B.Y); }
+        public static Int2 operator *(Int2 _A, Int2 _B) { return new Int2(_A.X * _B.X, _A.Y * _B.Y); }
+        public static Int2 operator *(Int2 _A, int _B) { return new Int2(_A.X * _B, _A.Y * _B); }
+        public static Int2 operator /(Int2 _A, Int2 _B) { return new Int2(_A.X / _B.X, _A.Y / _B.Y); }
+        public static Int2 operator /(Int2 _A, int _B) { return new Int2(_A.X / _B, _A.Y / _B); }
+        public static Int2 operator -(Int2 _A) { return new Int2(-_A.X, -_A.Y); }
     }
 
     public struct Float2
@@ -65,54 +29,18 @@ namespace SynergyTemplate
         public float X;
         public float Y;
 
-        public Float2(float _X, float _Y)
-        {
-            X = _X; Y = _Y;
-        }
-
-        public static bool operator <(Float2 _A, Float2 _B)
-        {
-            return _A.X < _B.X && _A.Y < _B.Y;
-        }
-        public static bool operator >(Float2 _A, Float2 _B)
-        {
-            return _A.X > _B.X && _A.Y > _B.Y;
-        }
-
-        public static Float2 operator +(Float2 _A, Float2 _B)
-        {
-            return new Float2(_A.X + _B.X, _A.Y + _B.Y);
-        }
-
-        public static Float2 operator -(Float2 _A, Float2 _B)
-        {
-            return new Float2(_A.X - _B.X, _A.Y - _B.Y);
-        }
-
-        public static Float2 operator *(Float2 _A, Float2 _B)
-        {
-            return new Float2(_A.X * _B.X, _A.Y * _B.Y);
-        }
-
-        public static Float2 operator *(Float2 _A, float _B)
-        {
-            return new Float2(_A.X * _B, _A.Y * _B);
-        }
-
-        public static Float2 operator /(Float2 _A, Float2 _B)
-        {
-            return new Float2(_A.X / _B.X, _A.Y / _B.Y);
-        }
-
-        public static Float2 operator /(Float2 _A, float _B)
-        {
-            return new Float2(_A.X / _B, _A.Y / _B);
-        }
-
-        public static Float2 operator -(Float2 _A)
-        {
-            return new Float2(-_A.X, -_A.Y);
-        }
+        public Float2(float _X, float _Y) { X = _X; Y = _Y; }
+        public static bool operator <(Float2 _A, Float2 _B) { return _A.X < _B.X && _A.Y < _B.Y; }
+        public static bool operator >(Float2 _A, Float2 _B) { return _A.X > _B.X && _A.Y > _B.Y; }
+        public static bool operator ==(Float2 _A, Float2 _B) { return _A.X == _B.X && _B.Y == _B.Y; }
+        public static bool operator !=(Float2 _A, Float2 _B) { return !(_A == _B); }
+        public static Float2 operator +(Float2 _A, Float2 _B) { return new Float2(_A.X + _B.X, _A.Y + _B.Y); }
+        public static Float2 operator -(Float2 _A, Float2 _B) { return new Float2(_A.X - _B.X, _A.Y - _B.Y); }
+        public static Float2 operator *(Float2 _A, Float2 _B) { return new Float2(_A.X * _B.X, _A.Y * _B.Y); }
+        public static Float2 operator *(Float2 _A, float _B) { return new Float2(_A.X * _B, _A.Y * _B); }
+        public static Float2 operator /(Float2 _A, Float2 _B) { return new Float2(_A.X / _B.X, _A.Y / _B.Y); }
+        public static Float2 operator /(Float2 _A, float _B) { return new Float2(_A.X / _B, _A.Y / _B); }
+        public static Float2 operator -(Float2 _A) { return new Float2(-_A.X, -_A.Y); }
     }
 
     public struct Float3
@@ -120,6 +48,7 @@ namespace SynergyTemplate
         public float X;
         public float Y;
         public float Z;
+
         public Float3(float _X, float _Y, float _Z) { X = _X; Y = _Y; Z = _Z; }
         public static bool operator <(Float3 _A, Float3 _B) { return _A.X < _B.X && _A.Y < _B.Y && _A.Z < _B.Z; }
         public static bool operator >(Float3 _A, Float3 _B) { return _A.X > _B.X && _A.Y > _B.Y && _A.Z > _B.Z; }
@@ -140,53 +69,15 @@ namespace SynergyTemplate
         public float Z;
         public float W;
 
-        public Float4(float _X, float _Y, float _Z, float _W)
-        {
-            X = _X; Y = _Y; Z = _Z; W = _W;
-        }
-
-        public static bool operator <(Float4 _A, Float4 _B)
-        {
-            return _A.X < _B.X && _A.Y < _B.Y && _A.Z < _B.Z && _A.W < _B.W;
-        }
-        public static bool operator >(Float4 _A, Float4 _B)
-        {
-            return _A.X > _B.X && _A.Y > _B.Y && _A.Z > _B.Z && _A.W > _B.W;
-        }
-
-        public static Float4 operator +(Float4 _A, Float4 _B)
-        {
-            return new Float4(_A.X + _B.X, _A.Y + _B.Y, _A.Z + _B.Z, _A.W + _B.W);
-        }
-
-        public static Float4 operator -(Float4 _A, Float4 _B)
-        {
-            return new Float4(_A.X - _B.X, _A.Y - _B.Y, _A.Z - _B.Z, _A.W - _B.W);
-        }
-
-        public static Float4 operator *(Float4 _A, Float4 _B)
-        {
-            return new Float4(_A.X * _B.X, _A.Y * _B.Y, _A.Z - _B.Z, _A.W - _B.W);
-        }
-
-        public static Float4 operator *(Float4 _A, float _B)
-        {
-            return new Float4(_A.X * _B, _A.Y * _B, _A.Z * _B, _A.W * _B);
-        }
-
-        public static Float4 operator /(Float4 _A, Float4 _B)
-        {
-            return new Float4(_A.X / _B.X, _A.Y / _B.Y, _A.Z / _B.Z, _A.W / _B.W);
-        }
-
-        public static Float4 operator /(Float4 _A, float _B)
-        {
-            return new Float4(_A.X / _B, _A.Y / _B, _A.Z / _B, _A.W / _B);
-        }
-
-        public static Float4 operator -(Float4 _A)
-        {
-            return new Float4(-_A.X, -_A.Y, -_A.Z, -_A.W);
-        }
+        public Float4(float _X, float _Y, float _Z, float _W) { X = _X; Y = _Y; Z = _Z; W = _W; }
+        public static bool operator <(Float4 _A, Float4 _B) { return _A.X < _B.X && _A.Y < _B.Y && _A.Z < _B.Z && _A.W < _B.W; }
+        public static bool operator >(Float4 _A, Float4 _B) { return _A.X > _B.X && _A.Y > _B.Y && _A.Z > _B.Z && _A.W > _B.W; }
+        public static Float4 operator +(Float4 _A, Float4 _B) { return new Float4(_A.X + _B.X, _A.Y + _B.Y, _A.Z + _B.Z, _A.W + _B.W); }
+        public static Float4 operator -(Float4 _A, Float4 _B) { return new Float4(_A.X - _B.X, _A.Y - _B.Y, _A.Z - _B.Z, _A.W - _B.W); }
+        public static Float4 operator *(Float4 _A, Float4 _B) { return new Float4(_A.X * _B.X, _A.Y * _B.Y, _A.Z - _B.Z, _A.W - _B.W); }
+        public static Float4 operator *(Float4 _A, float _B) { return new Float4(_A.X * _B, _A.Y * _B, _A.Z * _B, _A.W * _B); }
+        public static Float4 operator /(Float4 _A, Float4 _B) { return new Float4(_A.X / _B.X, _A.Y / _B.Y, _A.Z / _B.Z, _A.W / _B.W); }
+        public static Float4 operator /(Float4 _A, float _B) { return new Float4(_A.X / _B, _A.Y / _B, _A.Z / _B, _A.W / _B); }
+        public static Float4 operator -(Float4 _A) { return new Float4(-_A.X, -_A.Y, -_A.Z, -_A.W); }
     }
 }
