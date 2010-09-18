7ef3cd8f80a474cda2e1ff77117ef034f0410fb8
roenyroeny <roenyroeny@57e7344c-89cc-11de-832f-d15e35d84da2>
2010-09-18 18:40:11 +0000

    added new mainstation firmware

---
+++ trunk/Code/Hardware/MainStation/UART.c
@@ -0,0 +1,128 @@
+#include"Default.h"
+#include"UART.h"
+
+#include "Compiler.h"
+
+#include "HardwareProfile.h"
+
+#define TIMER
+
+
+void UARTRead()
+{
+	while(!TXSTAbits.TRMT);//wait for last write to complete
+	UART_DIR=0;
+	PIR1bits.RCIF=0;
+	RCSTAbits.CREN=0;
+	RCSTAbits.CREN=1;
+	RCREG=RCREG;
+}
+
+void UARTInit()
+{
+	TXSTAbits.SYNC=0;						//asynchronous
+	RCSTAbits.SPEN=1;						//enable serial port pins
+	RCSTAbits.CREN=1;						//enable reception
+	RCSTAbits.SREN=0;						//no effect
+	TXSTAbits.TX9=0;						//8-bit transmission
+	RCSTAbits.RX9=0;						//8-bit reception
+	TXSTAbits.TXEN=0;						//reset transmitter
+	TXSTAbits.TXEN=1;						//enable the transmitter
+
+	UART_RX_TRIS	=1;			//input
+	UART_TX_TRIS	=0;			//output
+	UART_DIR_TRIS	=1;			//output
+	UART_DIR=0;					//read by default
+
+	//bitrate = 10417
+	BAUDCONbits.BRG16=1;
+	TXSTAbits.BRGH=0;
+
+	SPBRGH=4;
+	SPBRG =0;
+
+	UART_DIR_TRIS=0;
+
+	//enable timer
+	T0CONbits.PSA=0;
+	T0CONbits.T0PS0=1;
+	T0CONbits.T0PS1=1;
+	T0CONbits.T0PS2=1;
+	T0CONbits.T08BIT=1;
+	T0CONbits.TMR0ON=1;
+	T0CONbits.T0CS=0;
+
+	UARTRead();
+}
+
+int8 UARTReadInt8(void)
+{
+#ifdef TIMER
+	TMR0L=0;
+	INTCONbits.TMR0IF=0;
+#endif
+	while(!PIR1bits.RCIF)
+	{
+#ifdef TIMER
+		if(INTCONbits.TMR0IF)
+		{
+			UARTError=255;
+			return 0;
+		}
+#endif
+		UARTClearErrors;
+	}
+	return RCREG;
+}
+
+int16 UARTReadInt16()
+{
+	short var;
+	char*dat=&var;
+	dat[1]=UARTReadInt8();
+	dat[0]=UARTReadInt8();
+	return var;
+}
+
+int8 UARTAvailable()
+{
+  if (PIR1bits.RCIF) return 255;
+  return 0;
+}
+
+void UARTWrite()
+{
+	//give clients a chance to go in read mode
+	TMR0L=0;
+	INTCONbits.TMR0IF=0;
+	while(!INTCONbits.TMR0IF);
+
+	UART_DIR=1;
+	PIR1bits.TXIF=0;
+	TXSTAbits.TXEN=0;
+	TXSTAbits.TXEN=1;
+}
+
+void UARTWriteInt8(int8 c)
+{
+	int i;
+	while(!PIR1bits.TXIF)UARTClearErrors;
+	TXREG=c;
+	while(!PIR1bits.TXIF)UARTClearErrors;
+}
+
+void UARTWriteString(register const char *str)
+{
+	while((*str)!=0)
+	{
+	 	UARTWriteInt8(*str);
+		str++;
+	}
+}
+
+void UARTWriteInt16(int16 _Value)
+{
+	int8*dat = &_Value;
+	UARTWriteInt8(dat[1]);
+	UARTWriteInt8(dat[0]);
+}
---
+++ trunk/Code/Hardware/MainStation/USB.c
@@ -0,0 +1,108 @@
+#include "Default.h"
+
+#include "USB.h"
+/*
+#include "usb_config.h"
+#include "./USB/usb_device.h"
+#include "./USB/usb.h"
+#include "./USB/usb_function_cdc.h"
+
+#include "usb_config.h"
+#include "USB\usb_device.h"
+#include "USB\usb.h"
+
+#define USBBUFFERSIZE 64
+
+int8 USBOutBuffer[USBBUFFERSIZE];
+int8 USBOutIndex=0;
+
+int8 USBInBuffer[USBBUFFERSIZE];
+int8 USBInIndex=0;
+int8 USBInSize=0;
+
+int8 USBPollDelay=0;
+
+void USBInit()
+{
+	unsigned char i;
+	USBDeviceAttach();
+}
+void USBUpdate()
+{
+	//if(USBPollDelay&32)
+	{
+		USBDeviceTasks();
+		CDCTxService();
+	}
+	USBPollDelay++;
+}
+
+int8 USBCanWrite()
+{
+	if((USBDeviceState < CONFIGURED_STATE)||(USBSuspendControl==1))
+		return 0;
+	return 255;
+}
+
+int8 USBForceWrite()
+{
+	if(!mUSBUSARTIsTxTrfReady())
+	{
+		return 0;
+	}
+	if(USBOutIndex!=0)
+	{
+		putUSBUSART(USBOutBuffer, USBOutIndex);
+		CDCTxService();
+		USBOutIndex=0;
+	}
+	return 255;
+}
+void USBWriteInt8(const int8 _Data)
+{
+	USBOutBuffer[USBOutIndex]=_Data;
+	USBOutIndex++;
+	if(USBOutIndex==USBBUFFERSIZE)
+	{
+		USBForceWrite();
+	}
+}
+
+int8 USBCanRead()
+{
+	if(USBInIndex>=USBInSize||USBInIndex>=USBBUFFERSIZE)
+	{
+		USBInSize = getsUSBUSART(&USBInBuffer[0],USBBUFFERSIZE);
+		USBInIndex=0;
+		if(USBInSize==0)return 0;
+	}
+	return 255;
+}
+
+int8 USBReadInt8()
+{
+	int8 d;
+	if(!USBCanRead())return 0;
+	d = USBInBuffer[USBInIndex];
+	USBInIndex++;
+	return d;
+}
+
+int16 USBReadInt16()
+{
+	union
+	{
+		int16 ret;
+		struct
+		{
+			int8 dat1;
+			int8 dat2;
+		}st;
+	}un;
+
+	un.st.dat1=USBReadInt8();
+	un.st.dat2=USBReadInt8();
+
+	return un.ret;
+}
+*/
