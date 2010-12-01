33d1feb0338c61d06b62788932080684669a260b
roenyroeny <roenyroeny@57e7344c-89cc-11de-832f-d15e35d84da2>
2010-12-01 14:04:10 +0000

    +Added webserver project

---
+++ trunk/Code/WebServer/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text;
+using System.Net;
+using System.Drawing;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WebServer
+{
+    class Program
+    {
+        public static Scene scene = new Scene();
+
+        public static Size Resolution = new Size(240, 320);
+
+        public static Image backgroundimage;
+        public static Image On;
+        public static Image Off;
+
+        public static HttpListener listener;
+        static void Main(string[] args)
+        {
+            if (!HttpListener.IsSupported) Console.WriteLine("HTTPListener class is not supported on your machine.");
+
+            scene.BackgroundImage = Image.FromFile("background.png");
+            scene.DefaultOnImage = Image.FromFile("On.png");
+            scene.DefaultOffImage = Image.FromFile("Off.png");
+
+
+            scene.ClickablePoints.Add(new Scene.ClickableRegion(40.0f / 320, 40.0f / 480, 0.2f, "Button1"));
+            scene.ClickablePoints.Add(new Scene.ClickableRegion(200.0f / 320, 300.0f / 480, 0.2f, "Button2"));
+
+            scene.ClickablePoints.Add(new Scene.ClickableRegion(160.0f / 320, 120.0f / 480, 0.2f, "Button3"));
+            scene.ClickablePoints.Add(new Scene.ClickableRegion(200.0f / 320, 200.0f / 480, 0.2f, "Button4"));
+
+
+            // Create a listener.
+            listener = new HttpListener();
+            //listener.AuthenticationSchemes = AuthenticationSchemes.Basic;//might be phun
+
+            string LocalIP = Dns.GetHostName();
+            Console.WriteLine("HostName:" + LocalIP);
+            LocalIP = Dns.GetHostByName(LocalIP).AddressList[0].ToString();
+            Console.WriteLine("HostIP:" + LocalIP);
+            listener.Prefixes.Add("http://" + LocalIP + ":1000/");
+
+            listener.Start();
+            Console.WriteLine("Listening...");
+            Thread thread = new Thread(new ThreadStart(listen));
+            thread.Start();
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+            listener.Stop();
+        }
+
+        static void listen()
+        {
+            while (true)
+            {
+                try
+                {
+                    // Note: The GetContext method blocks while waiting for a request. 
+                    HttpListenerContext context = listener.GetContext();
+                    //check username/password
+                    if (context.User != null && context.User.Identity is HttpListenerBasicIdentity)
+                    {
+                        HttpListenerBasicIdentity identity = (HttpListenerBasicIdentity)context.User.Identity;
+                        if (identity.Name != "Roeny" && identity.Password != "Baloony") continue;
+                    }
+
+                    HttpListenerRequest request = context.Request;
+                    string parameters = request.RawUrl.Replace("?", "");
+                    // Obtain a response object.
+                    HttpListenerResponse response = context.Response;
+                    //handle button press
+                    foreach (Scene.ClickableRegion p in scene.ClickablePoints)
+                    {
+                        if (parameters == "/" + p.Target)
+                        {
+                            p.Pressed = !p.Pressed;
+                            Console.WriteLine(p.Target + " Pressed");
+                        }
+                    }
+
+                    string ResponseString = scene.GetHTML(new Scene.Settings());
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(ResponseString);
+
+                    if (parameters.Contains("background"))
+                    {
+                        Image bitmap = scene.GetImage(new Scene.Settings());
+                        System.IO.MemoryStream stream = new System.IO.MemoryStream();
+                        bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        buffer = stream.ToArray();
+                    }
+                    // Get a response stream and write the response to it.
+                    response.ContentLength64 = buffer.Length;
+                    System.IO.Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+                    // You must close the output stream.
+                    output.Close();
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+        }
+    }
+}
---
+++ trunk/Code/WebServer/Scene.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace WebServer
+{
+    public class Scene
+    {
+        public class Settings
+        {
+            public Settings()
+            {
+                Width = 320;
+                Height = 480;
+                Detail = 1;
+            }
+            public Settings(int _Width, int _Height, int _Detial)
+            {
+                Width = _Width;
+                Height = _Height;
+                Detail = _Detial;
+            }
+            public int Width, Height, Detail;
+        }
+
+        public class ClickableRegion
+        {
+            public ClickableRegion(float _X, float _Y, float _Radius, string _Target)
+            {
+                X = _X;
+                Y = _Y;
+                Radius = _Radius;
+                Target = _Target;
+            }
+            public float X, Y, Radius;
+            public string Target;
+            public bool Pressed;
+            public Image On = null;
+            public Image Off = null;
+
+            public int GetX(Settings _Settings)
+            {
+                return (int)(X * _Settings.Width);
+            }
+
+            public int GetY(Settings _Settings)
+            {
+                return (int)(Y * _Settings.Height);
+            }
+
+            public int GetRadius(Settings _Settings)
+            {
+                return (int)(Radius * _Settings.Width);
+            }
+        }
+
+        public Image BackgroundImage;
+        public Image DefaultOnImage;
+        public Image DefaultOffImage;
+
+        public List<ClickableRegion> ClickablePoints = new List<ClickableRegion>();
+
+        public string GetHTML(Settings _Settings)
+        {
+            string Areas = "";
+            foreach (Scene.ClickableRegion p in ClickablePoints)
+            {
+                Areas += string.Format("<area shape=\"circle\" coords=\"{0},{1},{2}\" href=\"{3}\" alt=\"{4}\" />", p.GetX(_Settings), p.GetY(_Settings), p.GetRadius(_Settings) / 2, p.Target, p.Target);
+            }
+            string response = System.IO.File.ReadAllText("index.htm");
+            response = response.Replace("<<<<AREAS>>>>", Areas);
+            string backgroundname = "background-" + Guid.NewGuid().ToString();
+            response = response.Replace("<<<<BGNAME>>>>", backgroundname);
+            response = response.Replace("<<<<WIDTH>>>>", _Settings.Width.ToString());
+            response = response.Replace("<<<<HEIGHT>>>>", _Settings.Height.ToString());
+            return response;
+        }
+
+        public Image GetImage(Settings _Settings)
+        {
+            Bitmap b = new Bitmap(_Settings.Width / _Settings.Detail, _Settings.Height / _Settings.Detail);
+            Graphics graphics = Graphics.FromImage(b);
+            graphics.DrawImage(BackgroundImage, 0, 0, b.Width, b.Height);
+            foreach (ClickableRegion p in ClickablePoints)
+            {
+                Image on = p.On != null ? p.On : DefaultOnImage;
+                Image off = p.Off != null ? p.Off : DefaultOffImage;
+                graphics.DrawImage(p.Pressed ? on : off,
+                    new RectangleF(
+                        (p.GetX(_Settings) - p.GetRadius(_Settings) / 2.0f) / _Settings.Detail,
+                        (p.GetY(_Settings) - p.GetRadius(_Settings) / 2.0f) / _Settings.Detail,
+                        p.GetRadius(_Settings) / _Settings.Detail, p.GetRadius(_Settings) / _Settings.Detail));
+            }
+
+            return (Image)b;
+        }
+    }
+}
