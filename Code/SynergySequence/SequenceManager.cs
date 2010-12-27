d2a522baafc32bc0a87c44d1494e6f82e1a057c8
roenyroeny <roenyroeny@57e7344c-89cc-11de-832f-d15e35d84da2>
2010-12-27 12:55:15 +0000

    oops..

---
+++ Code/SynergySequence/SequenceManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace SynergySequence
+{
+    public abstract class SequenceManager
+    {
+        List<Prototype> prototypes = new List<Prototype>();
+        public Prototype[] Prototypes { get { return prototypes.ToArray(); } }
+        public void AddPrototype(Prototype _Prototype) { prototypes.Add(_Prototype); }
+
+        List<DataType> datatypes = new List<DataType>();
+        public DataType[] DataTypes { get { return datatypes.ToArray(); } }
+        public void AddDataType(DataType _DataType) { datatypes.Add(_DataType); }
+
+        public SequenceManager()
+        {
+
+        }
+
+        public DataType GetDataType(string _DataTypeName)
+        {
+            foreach (DataType t in datatypes) if (t.Name == _DataTypeName) return t;
+            return null;
+        }
+
+        public Prototype GetProtoType(string _TypeName)
+        {
+            foreach (Prototype p in prototypes)
+            {
+                if (p.BlockType.FullName == _TypeName) return p;
+            }
+            return null;
+        }
+
+        public CodeBlock CreateCodeBlock(string _TypeName) { return CreateCodeBlock(GetProtoType(_TypeName)); }
+        public abstract CodeBlock CreateCodeBlock(Prototype _Prototype);
+
+        public class DataType
+        {
+            public DataType(string _Name, Color _Color)
+            {
+                Name = _Name;
+                Color = _Color;
+            }
+            public string Name;
+            public Color Color;
+        }
+
+        public class Prototype
+        {
+            public Prototype(string _Name, string _GroupName, string _Description, Type _BlockType)
+            {
+                Name = _Name; GroupName = _GroupName; Description = _Description; BlockType = _BlockType; UserCanAdd = true;
+            }
+            public Prototype(string _Name, string _GroupName, string _Description, Type _BlockType, bool _UserCanAdd)
+            {
+                Name = _Name; GroupName = _GroupName; Description = _Description; BlockType = _BlockType; UserCanAdd = _UserCanAdd;
+            }
+            public string Name;
+            public string GroupName;
+            public string Description;
+            public Type BlockType;
+            public bool UserCanAdd;
+            public object Create() { return Activator.CreateInstance(null, BlockType); }
+        }
+    }
+}
