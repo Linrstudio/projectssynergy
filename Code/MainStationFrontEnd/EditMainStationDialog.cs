ffd71ee42fb53f7e36a930473cc30923609f4c4e
roenyroeny <roenyroeny@gmail.com>
2011-03-10 20:33:04 +0000

    oops

---
+++ Code/MainStationFrontEnd/EditMainStationDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MainStationFrontEnd
+{
+    public partial class EditMainStationDialog : Form
+    {
+        FrontEndMainStation mainstation;
+        public EditMainStationDialog(FrontEndMainStation _MainStation)
+        {
+            mainstation = _MainStation;
+            InitializeComponent();
+        }
+
+        private void EditMainStation_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form = new SynergySequence.SequenceEditorForm(mainstation.Sequence);
+            form.Tag = mainstation.Sequence;
+            MainWindow.mainwindow.ShowDialog(form);
+        }
+    }
+}
